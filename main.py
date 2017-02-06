aad466c9137634c0aeda89428bc600bd8576db3f
inspiredxstem <scripteduser@Codes-MacBook-Air.local>
2017-02-06 17:48:47 -0500

    Almost finished
    Really close to completion

--- main.py
+++ main.py
@@ -14,39 +14,74 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
 import csv
-import StringIO
 import json
+import StringIO
+import webapp2
 
+from google.appengine.ext import ndb
 
-# class MainHandler(webapp2.RequestHandler):
-#     def get(self):
-#         self.response.write('Hello world!')
-
-def process_csv_data(csv_data):
+def csv_string_to_dict(csv_data):
     precinct_to_votes = {}
     is_header = True
     f = StringIO.StringIO(csv_data)
     rows = csv.reader(f, delimiter=',')
     for row in rows:
         if is_header:
-            is_header = False
-            continue
+          is_header = False
+          continue
         office = row[2]
-        if office == "President":
-            precinct = row[1]
-            
+        if office != 'President':
+          continue
+        county = row[0]
+        precinct = row[1]
+        key = county + '__' + precinct
+        entry = precinct_to_votes.get(key, {})
+        precinct_to_votes[key] = entry
+        entry['county'] = county
+        entry['precinct'] = precinct
+
+        precinct_votes = entry.get('votes', {})
+        entry['votes'] = precinct_votes
+
+        candidate = row[5]
+        votes = int(row[6])
+        if candidate in precinct_votes:
+          precinct_votes[candidate] += votes
+        else:
+          precinct_votes[candidate] = votes
     return precinct_to_votes
+
+class PrecinctVotes(ndb.Model):
+    precinct = ndb.StringProperty()
+    county = ndb.StringProperty()
+    votes = ndb.JsonProperty()
 
 class UploadHandler(webapp2.RequestHandler):
     def post(self):
         csv_data = self.request.POST.get('csv_file').file.read()
-        results = process_csv_data(csv_data)
-        for key in results:
-            self.response.out.write(json.dumps(results[key]) + "<br>")
+        precinct_to_votes = csv_string_to_dict(csv_data)
+        for key in precinct_to_votes:
+            entry = precinct_to_votes[key]
+            precinct_id = entry['county'] + '__' + entry['precinct']
+            new_precinct = PrecinctVotes(
+                id = precinct_id,
+                county = entry["county"],
+                precinct = entry["precinct"],
+                votes = entry["votes"]
+            )
+            new_precinct.put()
+        self.response.out.write('Upload successful')
+
+class PrecinctHandler(webapp2.RequestHandler):
+    def get(self):
+        results = []
+        for precinct in PrecinctVotes.query():
+            results.append(precinct.to_dict())
+        self.response.out.write(json.dumps(results))
+        self.response.headers.add_header('Content-Type', 'application/json')
 
 app = webapp2.WSGIApplication([
-    # ('/', MainHandler),
-    ('/upload', UploadHandler)
+    ('/upload', UploadHandler),
+    ('/precincts', PrecinctHandler)
 ], debug=True)
